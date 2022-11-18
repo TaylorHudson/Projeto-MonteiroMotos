381cfc5ca48f983f4c7bc79cd8b227672f41472d
Taylor Hudson <taylorhudsonsantos@gmail.com>
2022-11-18 11:16:59 -0300

    Finaliza tela home mototaxista

--- projeto/src/main/java/projeto/telas/mototaxista/TelaHomeMototaxista.java
+++ projeto/src/main/java/projeto/telas/mototaxista/TelaHomeMototaxista.java
@@ -1,21 +1,28 @@
 package projeto.telas.mototaxista;
 
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
 import utilidades.fabricas.FabricaJButton;
+import utilidades.fabricas.FabricaJLabel;
 import utilidades.imagens.Imagens;
 
 public class TelaHomeMototaxista extends JFrame{
-
+	private static final long serialVersionUID = 1L;
+	
 	private JMenu editar;
 	private JMenu deletar;
 	private JButton btnListarCorridas;
 	private JButton btnComprarCreditos;
+	private JLabel background;
 
 	public TelaHomeMototaxista(){
+		configImagemFundo();
 		configurarTela();
 		configMenu();
 		configBotoes();
@@ -42,11 +49,20 @@ public class TelaHomeMototaxista extends JFrame{
 		setJMenuBar(menuBar);
 	}
 	
+	private void configImagemFundo() {
+		background = FabricaJLabel.criarJLabel(0, 0, 900, 800, Imagens.BACKGROUNDHOME);
+		add(background);
+	}
+	
 	private void configBotoes() {
-		btnListarCorridas = FabricaJButton.criarJButton("Listar Corridas",Imagens.MOTOTAXISTA, 150, 20, 560, 380);
-		btnComprarCreditos = new JButton();
+		btnListarCorridas = FabricaJButton.criarJButton("Listar Corridas", 150, 220, 560, 230,
+				new Color(28, 28, 20), new Color(179, 177, 177), 28);
 		
-		add(btnListarCorridas);
+		btnComprarCreditos = FabricaJButton.criarJButton("Comprar Créditos", 150, 490, 560, 230,
+				new Color(28, 28, 20), new Color(179, 177, 177), 28);
+		
+		background.add(btnListarCorridas);
+		background.add(btnComprarCreditos);
 	}
 	
 	public static void main(String[] args) {
