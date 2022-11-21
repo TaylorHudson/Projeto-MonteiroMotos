1594fa8db62f453f383a85a2f5792ac5a30f99b3
Taylor Hudson <taylorhudsonsantos@gmail.com>
2022-11-21 10:37:24 -0300

    Cria a tela de listar corridas do mototaxista

---
+++ projeto/src/main/java/projeto/telas/mototaxista/TelaListarCorridas.java
@@ -0,0 +1,70 @@
+package projeto.telas.mototaxista;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import utilidades.fabricas.FabricaJButton;
+import utilidades.fabricas.FabricaJLabel;
+import utilidades.imagens.Imagens;
+
+import java.awt.Font;
+import java.awt.Color;
+
+public class TelaListarCorridas extends JFrame {
+
+  private JTable tabelaCorridas;
+  private JLabel background;
+  private JButton btnOrdenar;
+  private JButton btnReivindicarCorrida;
+  
+  public TelaListarCorridas() {
+    configurarTela();
+    configImagemFundo();
+    configTabela();
+
+    setVisible(true);
+  }
+
+  private void configImagemFundo() {
+		background = FabricaJLabel.criarJLabel(0, 0, 900, 800, Imagens.BACKGROUNDHOME);
+		add(background);
+	}
+
+  private void configTabela() {
+    DefaultTableModel modelo = new DefaultTableModel();
+		modelo.setColumnIdentifiers(new String[] { "NOME", "STATUS", "DATA", "HORA" });
+		tabelaCorridas = new JTable(modelo);
+		tabelaCorridas.setFont(new Font("Arial", 1, 15));
+
+		JScrollPane scrol = new JScrollPane(tabelaCorridas);
+		scrol.getViewport().setBackground(new Color(124, 68, 2));
+		scrol.setBounds(2, 240, 885, 400);
+
+    btnOrdenar = FabricaJButton.criarJButton("Ordenar", 660, 180, 180, 50, new Color(28, 28, 20),
+    new Color(179, 177, 177), 28);
+   
+    btnReivindicarCorrida = FabricaJButton.criarJButton("Reivindicar Corrida", 50, 670, 280, 50, new Color(28, 28, 20),
+    new Color(179, 177, 177), 28);
+
+    background.add(scrol);
+    background.add(btnOrdenar);
+    background.add(btnReivindicarCorrida);
+  }
+
+  private void configurarTela() {
+		setSize(900, 800);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setTitle("Tela Listar Corridas");
+	}
+
+  public static void main(String[] args) {
+    new TelaListarCorridas();
+  }
+}
